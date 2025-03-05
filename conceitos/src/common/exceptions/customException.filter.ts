90a6ba3f085c489d0557b191829426fb067e3c58
Wellington Pinho <wellpinho@outlook.com>
2025-03-05 14:16:37 -0300

    exception filters

---
+++ conceitos/src/common/exceptions/customException.filter.ts
@@ -0,0 +1,8 @@
+// import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+
+// @Catch()
+// export class CustomExceptionFilter implements ExceptionFilter {
+//   catch(exception: any, host: ArgumentsHost) {
+//     throw new Error('Method not implemented');
+//   }
+// }
