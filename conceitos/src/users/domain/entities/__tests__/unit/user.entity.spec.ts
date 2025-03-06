4e3b6057637dc0dbe0dda29804b5d1a039e8f767
Wellington Pinho <wellpinho@outlook.com>
2025-03-06 20:48:12 -0300

    Adding UserDataBuilder helper method

--- conceitos/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ conceitos/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,16 +1,11 @@
-import { faker } from '@faker-js/faker';
 import { UserEntity, UserProps } from '../../user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
 describe('UserEntity', () => {
   let props: UserProps;
   let sut: UserEntity;
 
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-
+    props = UserDataBuilder({});
     sut = new UserEntity(props);
   });
 
