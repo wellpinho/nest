1f66e4e922a1bb6226ec6386d02e19e2f1c4b065
Wellington Pinho <wellpinho@outlook.com>
2025-03-06 20:42:22 -0300

    UserEntity - getters methods

--- conceitos/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ conceitos/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -15,9 +15,32 @@ describe('UserEntity', () => {
   });
 
   it('Constructor methos', () => {
-    expect(sut.props.name).toEqual(props.name);
-    expect(sut.props.email).toEqual(props.email);
-    expect(sut.props.password).toEqual(props.password);
+    expect(sut.props.name).toStrictEqual(props.name);
+    expect(sut.props.email).toStrictEqual(props.email);
+    expect(sut.props.password).toStrictEqual(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+
+  it('Getter of name field', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toStrictEqual(props.name);
+    expect(typeof sut.props.name).toStrictEqual('string');
+  });
+
+  it('Getter of email field', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toStrictEqual(props.email);
+    expect(typeof sut.props.email).toStrictEqual('string');
+  });
+
+  it('Getter of password field', () => {
+    expect(sut.props.password).toBeDefined();
+    expect(sut.props.password).toStrictEqual(props.password);
+    expect(typeof sut.props.password).toStrictEqual('string');
+  });
+
+  it('Getter of createdAt field', () => {
+    expect(sut.props.createdAt).toBeDefined();
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
 });
