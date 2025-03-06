5ed3dc7b1975f35e3c5632abe58a8b629fc12847
Wellington Pinho <wellpinho@outlook.com>
2025-03-06 20:28:03 -0300

    first test userEntity

---
+++ conceitos/src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker';
+import { UserEntity, UserProps } from '../../user.entity';
+describe('UserEntity', () => {
+  let props: UserProps;
+  let sut: UserEntity;
+
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    sut = new UserEntity(props);
+  });
+
+  it('Constructor methos', () => {
+    expect(sut.props.name).toEqual(props.name);
+    expect(sut.props.email).toEqual(props.email);
+    expect(sut.props.password).toEqual(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+});
--- conceitos/src/users/domain/entities/user.entity.ts
+++ conceitos/src/users/domain/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class UserEntity {}
+// TODO: aqui usa typescript puro sem nenhum uso de libs
+
+export type UserProps = {
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+};
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+}
